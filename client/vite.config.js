0940b84a3753d9488eb3260100a576f10f835a94
Torwai <iawrot085@gmail.com>
2026-08-22 15:41:52 +0700

    vchange

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react-router-dom')) return 'router';
-            if (id.includes('motion')) return 'motion';
-            if (id.includes('react') || id.includes('react-dom')) return 'vendor';
-          }
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          motion: ['motion'],
         }
       }
     },
