f1cc8bfba0aed9ae69432e26ce711c05f9b08821
Torwai <iawrot085@gmail.com>
2026-08-29 17:10:58 +0700

    update-01 add spaceblog link

--- client/src/Components/project/ProjectHeader.jsx
+++ client/src/Components/project/ProjectHeader.jsx
@@ -40,7 +40,7 @@ const ProjectHeader = () => {
         
         {id === '1' && 
         <div className='flex justify-center gap-2 mt-4 md:gap-3 md:mt-6'>
-          <motion.a className='flex items-center gap-[6px] border-2 border-border rounded-md px-1 py-[6px] text-primary text-sm font-regural cursor-pointer duration-100 hover:bg-secondary-bg/50 md:gap-2 md:px-2 md:text-md md:font-medium' href='/' aria-label={buttons.text} title={buttons.text} variants={animation[1]}>
+          <motion.a className='flex items-center gap-[6px] border-2 border-border rounded-md px-1 py-[6px] text-primary text-sm font-regural cursor-pointer duration-100 hover:bg-secondary-bg/50 md:gap-2 md:px-2 md:text-md md:font-medium' href='https://space-blog-delta.vercel.app' aria-label={buttons.text} title={buttons.text} variants={animation[1]}>
             <img className='w-5 h-5 md:w-7 md:h-7' alt={buttons[0].text} src={buttons[0].icon} />
             <span>{buttons[0].text}</span>
           </motion.a>
